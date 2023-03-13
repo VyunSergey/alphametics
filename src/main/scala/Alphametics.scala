3ebc519a0e36bdaa3f30303e01606f7e8c26b40c
Vyun Sergey <vyun.sergey@icloud.com>
2023-03-13 21:46:59 +0300

    add solution

--- src/main/scala/Alphametics.scala
+++ src/main/scala/Alphametics.scala
@@ -1,27 +1,89 @@
+import scala.annotation.tailrec
+
 object Alphametics {
+  val alphabet: Set[Char] = ('A' to 'Z').toSet
+  val digits: Set[Int] = (0 to 9).toSet
+
+  // MAIN solve method
+  def solve(eq: String): Option[Map[Char, Int]] =
+    eq.split(" == ").toList match {
+      case left :: right :: Nil if valid(left) && valid(right) => solveEq(left.split(" \\+ "), right)
+      case _ => None
+    }
+
+  // prepare method for creating list of equations to solve
+  def solveEq(left: Array[String], right: String): Option[Map[Char, Int]] = {
+    val leftEqs: List[List[(Char, Int)]] = equations(left)
+    val rightEqs: List[Option[Char]] = right.toList.reverse.map(Some(_))
+    val unionEqs: List[(List[(Char, Int)], Option[Char])] = leftEqs.zipAll(rightEqs, Nil, None)
+    val lts: Set[Char] = letters(left) ++ letters(Array(right))
+    val sts: Set[Char] = startLetters(left) ++ startLetters(Array(right))
+    val conditions: Map[Char, Set[Int]] = lts.map(c => c -> digits).toMap ++ sts.map(c => c -> (digits - 0)).toMap
+    solveEqs(unionEqs, 0, conditions, Map.empty[Char, Int])
+  }
+
+  // recursive method for solving list of equations from last digit to first
+  def solveEqs(eqs: List[(List[(Char, Int)], Option[Char])],
+               accum: Int,
+               conditions: Map[Char, Set[Int]],
+               known: Map[Char, Int]): Option[Map[Char, Int]] = {
+    eqs match {
+      case Nil =>
+        // known is final solution
+        Some(known)
+      case ((c, n) :: rest, Some(cr)) :: tail if known.contains(c) =>
+        // replace known char with digit
+        solveEqs((rest, Some(cr)) :: tail, accum + n * known(c), conditions, known)
+      case ((c, n) :: rest, Some(cr)) :: tail if !known.contains(c) =>
+        // iterate over all available digits for unknown char
+        (conditions(c) diff known.values.toSet)
+          .foldLeft(Option.empty[Map[Char, Int]]) { case (answer, digit) =>
+            answer orElse solveEqs((rest, Some(cr)) :: tail, accum + n * digit, conditions, known ++ Map(c -> digit))
+          }
+      case (Nil, Some(cr)) :: tail if known.contains(cr) =>
+        // check accum equals known digit for right side char
+        Some(cr).filter(accum % 10 == known(_)).flatMap { _ =>
+          solveEqs(tail, accum / 10, conditions, known)
+        }
+      case (Nil, Some(cr)) :: tail if !known.contains(cr) &&
+        (conditions(cr) diff known.values.toSet).contains(accum % 10) =>
+        // found digit for right side char
+        solveEqs(tail, accum / 10, conditions, known ++ Map(cr -> accum % 10))
+      case _ => None
+    }
+  }
+
+  // TEXT -> (A ... Z)[_+_(A ... Z)]*...
   def valid(part: String): Boolean = {
-    part.split(" \\+ ").forall(_.forall(c => c.isLetter && c.isUpper))
+    part.split(" \\+ ").forall(_.forall(c => alphabet.contains(c)))
   }
 
-  def startChars(part: String): Set[Char] = {
-    part.split(" \\+ ").map(_.head).toSet
+  // [[A, B], [C]] UNION [[C], [D, E], [F]] -> [[A, B, C], [C, D, E], [F]]
+  @tailrec
+  def union[A](left: List[List[A]], right: List[List[A]], res: List[List[A]] = Nil): List[List[A]] =
+    (left, right) match {
+      case (l :: ls, r :: rs) => union(ls, rs, (l ++ r) :: res)
+      case (lh :: ls, Nil)    => union(ls, Nil, lh :: res)
+      case (Nil, rh :: rs)    => union(Nil, rs, rh :: res)
+      case (Nil, Nil)         => res.reverse
+    }
+
+  // A + BB + AA + CDE -> [[(A,2), (B,1), (E,1)], [(B,1), (A,1), (D,1)], [(C,1)]]
+  def equations(parts: Array[String]): List[List[(Char, Int)]] = {
+    parts
+      .map(_.toList.map(c => List(c)).reverse)
+      .reduce(union(_, _))
+      .map(_.groupBy(x => x).view.mapValues(_.length).toList)
   }
 
-  def solve(eq: String): Option[Map[Char, Int]] = eq.split(" == ").toList match {
-    case left :: right :: Nil if valid(left) && valid(right) => solve(left, right)
-    case _ => None
+  // A + BB + CDE -> [A, B, C, D, E]
+  def letters(parts: Array[String]): Set[Char] = {
+    parts.flatMap(_.toCharArray).toSet
   }
 
-  def solve(left: String, right: String): Option[Map[Char, Int]] = {
-    val letters: Set[Char] = Set('A' to 'Z': _*) intersect (left.toSet union right.toSet)
-    val startLetters: Set[Char] = startChars(left) union startChars(right)
-    println(startLetters, letters)
-
-    val candidates: Map[Char, List[Int]] =
-      Map.from(letters.map(c => c -> List.from(0 to 9))) ++
-        Map.from(startLetters.map(c => c -> List.from(1 to 9)))
-    println(candidates)
-
-    None
+  // A + BB + CDE -> [A, B, C]
+  def startLetters(parts: Array[String]): Set[Char] = {
+    parts.flatMap(_.headOption).toSet
   }
+
 }
--- src/test/scala/AlphameticsTest.scala
+++ src/test/scala/AlphameticsTest.scala
@@ -81,10 +81,53 @@ class AlphameticsTest extends AnyFunSuite with Matchers {
             'T' -> 9)))
   }
 
+  test("puzzle with ten letters and a lot of addends") {
+    // pending
+    Alphametics.solve("SO + MANY + MORE + MEN + SEEM + TO + SAY + THAT + " +
+      "THEY + MAY + SOON + TRY + TO + STAY + AT + HOME + " +
+      "SO + AS + TO + SEE + OR + HEAR + THE + SAME + ONE + " +
+      "MAN + TRY + TO + MEET + THE + TEAM + ON + THE + " +
+      "MOON + AS + HE + HAS + AT + THE + OTHER + TEN == TESTS") should be(
+      Some(
+        Map('E' -> 0,
+            'N' -> 5,
+            'T' -> 8,
+            'Y' -> 4,
+            'A' -> 6,
+            'M' -> 3,
+            'H' -> 1,
+            'R' -> 2,
+            'O' -> 9,
+            'S' -> 7)))
+  }
+
   test("puzzle with ten letters and 199 addends") {
     // pending
     Alphametics.solve(
-      "THIS + A + FIRE + THEREFORE + FOR + ALL + HISTORIES + I + TELL + A + TALE + THAT + FALSIFIES + ITS + TITLE + TIS + A + LIE + THE + TALE + OF + THE + LAST + FIRE + HORSES + LATE + AFTER + THE + FIRST + FATHERS + FORESEE + THE + HORRORS + THE + LAST + FREE + TROLL + TERRIFIES + THE + HORSES + OF + FIRE + THE + TROLL + RESTS + AT + THE + HOLE + OF + LOSSES + IT + IS + THERE + THAT + SHE + STORES + ROLES + OF + LEATHERS + AFTER + SHE + SATISFIES + HER + HATE + OFF + THOSE + FEARS + A + TASTE + RISES + AS + SHE + HEARS + THE + LEAST + FAR + HORSE + THOSE + FAST + HORSES + THAT + FIRST + HEAR + THE + TROLL + FLEE + OFF + TO + THE + FOREST + THE + HORSES + THAT + ALERTS + RAISE + THE + STARES + OF + THE + OTHERS + AS + THE + TROLL + ASSAILS + AT + THE + TOTAL + SHIFT + HER + TEETH + TEAR + HOOF + OFF + TORSO + AS + THE + LAST + HORSE + FORFEITS + ITS + LIFE + THE + FIRST + FATHERS + HEAR + OF + THE + HORRORS + THEIR + FEARS + THAT + THE + FIRES + FOR + THEIR + FEASTS + ARREST + AS + THE + FIRST + FATHERS + RESETTLE + THE + LAST + OF + THE + FIRE + HORSES + THE + LAST + TROLL + HARASSES + THE + FOREST + HEART + FREE + AT + LAST + OF + THE + LAST + TROLL + ALL + OFFER + THEIR + FIRE + HEAT + TO + THE + ASSISTERS + FAR + OFF + THE + TROLL + FASTS + ITS + LIFE + SHORTER + AS + STARS + RISE + THE + HORSES + REST + SAFE + AFTER + ALL + SHARE + HOT + FISH + AS + THEIR + AFFILIATES + TAILOR + A + ROOFS + FOR + THEIR + SAFE == FORTRESSES") should be(
+      "THIS + A + FIRE + THEREFORE + FOR + ALL + HISTORIES + I + TELL + A + TALE + " +
+        "THAT + FALSIFIES + ITS + TITLE + TIS + A + LIE + THE + TALE + " +
+        "OF + THE + LAST + FIRE + HORSES + LATE + AFTER + THE + FIRST + " +
+        "FATHERS + FORESEE + THE + HORRORS + THE + LAST + FREE + TROLL + " +
+        "TERRIFIES + THE + HORSES + OF + FIRE + THE + TROLL + " +
+        "RESTS + AT + THE + HOLE + OF + LOSSES + IT + IS + THERE + " +
+        "THAT + SHE + STORES + ROLES + OF + LEATHERS + AFTER + SHE + " +
+        "SATISFIES + HER + HATE + OFF + THOSE + FEARS + A + TASTE + RISES + " +
+        "AS + SHE + HEARS + THE + LEAST + FAR + HORSE + THOSE + FAST + " +
+        "HORSES + THAT + FIRST + HEAR + THE + TROLL + FLEE + OFF + TO + " +
+        "THE + FOREST + THE + HORSES + THAT + ALERTS + RAISE + THE + STARES + " +
+        "OF + THE + OTHERS + AS + THE + TROLL + ASSAILS + AT + THE + TOTAL + " +
+        "SHIFT + HER + TEETH + TEAR + HOOF + OFF + TORSO + AS + THE + LAST + " +
+        "HORSE + FORFEITS + ITS + LIFE + THE + FIRST + FATHERS + HEAR + " +
+        "OF + THE + HORRORS + THEIR + FEARS + THAT + THE + FIRES + " +
+        "FOR + THEIR + FEASTS + ARREST + AS + THE + FIRST + FATHERS + " +
+        "RESETTLE + THE + LAST + OF + THE + FIRE + HORSES + THE + " +
+        "LAST + TROLL + HARASSES + THE + FOREST + HEART + FREE + " +
+        "AT + LAST + OF + THE + LAST + TROLL + ALL + OFFER + THEIR + " +
+        "FIRE + HEAT + TO + THE + ASSISTERS + FAR + OFF + THE + " +
+        "TROLL + FASTS + ITS + LIFE + SHORTER + AS + STARS + " +
+        "RISE + THE + HORSES + REST + SAFE + AFTER + ALL + SHARE + " +
+        "HOT + FISH + AS + THEIR + AFFILIATES + TAILOR + A + " +
+        "ROOFS + FOR + THEIR + SAFE == FORTRESSES") should be(
       Some(
         Map('A' -> 1,
             'E' -> 0,
